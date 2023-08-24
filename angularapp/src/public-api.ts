3dae2d3cc76dbde7f908fb27cb58c9b6308696b8
Iamneo <techadmin@iamneo.ai>
2023-08-24 15:10:53 +0000

    Backup Commit

---
+++ angularapp/src/public-api.ts
@@ -0,0 +1,6 @@
+/*
+ * Public API Surface of angular-currency-converter
+ */
+
+export * from './lib/components/currency-main/currency-main.component';
+export * from './lib/components/currency-main/currency-main.module';
